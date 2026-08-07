46eec7cf128b557f1a9e1816858b122e4bcaf619
bb <3237688681@qq.com>
2026-08-07 17:24:13 +0800

    用户端历史订单模块代码重构

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -25,4 +25,10 @@ public interface ShoppingCartMapper {
     //删除
     @Delete("delete from shopping_cart where id = #{id}")
     void deleteById(Long id);
+    /**
+     * 批量插入购物车数据
+     *
+     * @param shoppingCartList
+     */
+    void insertBatch(List<ShoppingCart> shoppingCartList);
 }
