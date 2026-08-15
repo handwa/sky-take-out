16394e174c58e185fba8b2d29de13ecb52b0e9b1
bb <3237688681@qq.com>
2026-08-15 23:39:29 +0800

    导出运营数据代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.time.LocalDate;
 
 
@@ -81,5 +83,11 @@ public class ReportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
         log.info("top10统计：{},{}", begin, end);
         return Result.success(reportService.getSalesTop10(begin, end));
+    }
+
+    //导出运营数据报表
+    @GetMapping("/export")
+    public void export(HttpServletResponse response) {
+        reportService.exportBusinessData(response);
     }
 }
